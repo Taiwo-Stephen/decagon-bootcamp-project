9a987511a023e8735ab22128728aee07717b67c8
Taiwo-Stephen <tbusari33@gmail.com>
2019-09-17 17:27:33 +0100

    third commit

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 $(function(){
+    $("#log-btn").click(function(){
+        const username = $("#username").val();
+        const password = $("#password").val();
 
-    $(".btn-primary").click(function(){
-        const outputfirstname = $("#inputFirstname").val();
-        const outputlastname = $("#inputLastname").val();
-        $("tbody").append("<tr><td> Delete </td> <td>" + outputfirstname + "</td> <td>" + outputlastname + "</td> <td> <input type='radio'> Present <input type='radio'>    Absent </td> </tr>");
-    });
-
-    
-});
+        if (username === "" || password) {
+            return "fill all space";
+        } else if (username === "admin" && password === "admin1234") {
+            
+        } {
+            
+        }
+        console.log("console");
+    })
+})
